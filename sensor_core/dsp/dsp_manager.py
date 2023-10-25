dae065455d950b0622b1819b8f5fb4dbe6040288
gracesosa <grace32214@gmail.com>
2023-10-25 18:27:00 -0400

    Added Moving avg & Butterworth filter functions

--- sensor_core/dsp/dsp_manager.py
+++ sensor_core/dsp/dsp_manager.py
@@ -1,3 +1,15 @@
 import numpy as np
+import scipy.signal as signal
 
-#TODO: add modules for 1D dsp 
+#Moving average filter
+def moving_avg_filter(data, N):
+    data_arr = np.concatenate(([np.min(data)] * (N), data))
+    cumsum = np.cumsum(data_arr)
+    yi = (cumsum[N:] - cumsum[:-N]) / float(N)
+    return yi
+    
+#Butterworth filter
+def butter_bandpass(data, min_frq, max_frq, N):
+    b, a = signal.butter(N, [min_frq, max_frq], 'band', analog = False, output = 'ba', fs = 100)
+    filtered = signal.filtfilt(b, a, data)
+    return filtered
