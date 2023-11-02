6eaa25bf4a5e608da3ae0edce66e4868c22bbc55
gracesosa <grace32214@gmail.com>
2023-11-02 19:57:12 -0400

    Renamed functions in DSP_manager and minor changes

--- sensor_core/dsp/dsp_manager.py
+++ sensor_core/dsp/dsp_manager.py
@@ -5,11 +5,11 @@ import scipy.signal as signal
 def moving_avg_filter(data, N):
     data_arr = np.concatenate(([np.min(data)] * (N), data))
     cumsum = np.cumsum(data_arr)
-    yi = (cumsum[N:] - cumsum[:-N]) / float(N)
-    return yi
+    filtered = (cumsum[N:] - cumsum[:-N]) / N
+    return filtered
     
 #Butterworth filter
-def butter_bandpass(data, min_frq, max_frq, N):
+def butter_filter(data, min_frq, max_frq, N):
     b, a = signal.butter(N, [min_frq, max_frq], 'band', analog = False, output = 'ba', fs = 100)
     filtered = signal.filtfilt(b, a, data)
     return filtered
