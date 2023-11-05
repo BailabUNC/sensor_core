5ca863c7db1682ac8a8b685aa29521ea431a39d1
gracesosa <grace32214@gmail.com>
2023-11-05 01:07:15 -0400

    added MA padding options, BW btype options

--- sensor_core/dsp/dsp_manager.py
+++ sensor_core/dsp/dsp_manager.py
@@ -2,14 +2,23 @@ import numpy as np
 import scipy.signal as signal
 
 #Moving average filter
-def moving_avg_filter(data, N):
-    data_arr = np.concatenate(([np.min(data)] * (N), data))
+def moving_avg_filter(data, N, pad):
+    if(pad == 'min'):
+        data_arr = np.concatenate(([np.min(data)] * (N), data))
+    elif(pad == 'percentile'):
+        data_arr = np.concatenate(([np.percentile(data, 10)] * (N), data))
+    else:
+        raise ValueError("Specify 'min' or 'percentile' to pad array with")
+    
     cumsum = np.cumsum(data_arr)
     filtered = (cumsum[N:] - cumsum[:-N]) / N
     return filtered
     
 #Butterworth filter
-def butter_filter(data, min_frq, max_frq, N):
-    b, a = signal.butter(N, [min_frq, max_frq], 'band', analog = False, output = 'ba', fs = 100)
+def butter_filter(data, min_frq, max_frq, N, btype):
+    if(btype != 'lowpass' or 'highpass' or 'bandpass' or 'bandstop'):
+        raise ValueError("Specify btype 'lowpass', 'highpass', 'bandpass', or 'bandstop'")
+    
+    b, a = signal.butter(N, [min_frq, max_frq], btype, analog = False, output = 'ba', fs = 100)
     filtered = signal.filtfilt(b, a, data)
     return filtered
